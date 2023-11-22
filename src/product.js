c4e045b4aa1eeac04df9ac8d14d7a1deecffe4fe
BachtiKhadija <151616256+BachtiKhadija@users.noreply.github.com>
2023-11-22 17:18:45 +0100

    Update product.js

--- src/product.js
+++ src/product.js
@@ -2,12 +2,13 @@ import React,{useState} from 'react'
 
 const Product = () => {
   const[data,setData]=useState([{id:1,title:'p1',price:30},{id:2,title:'p1',price:30},{id:3,title:'p1',price:30}])
+  
   return (
     <div>
        
 <ul>
        {
-        data.map(p=>(
+        data.filter(i=>i.title.includes('abc').map(p=>(
           <li key={p.id}>
             {p.title} ** {p.price}
           </li>
