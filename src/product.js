666531e8ad8cc6dbf7b17ddb50ea41a270050e70
myName <myEmail>
2023-11-22 17:05:42 +0100

    first modif in prduct.js

--- src/product.js
+++ src/product.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const product = () => {
+const Product = () => {
+  const[data,setData]=useState([{id:1,title:'p1',price:30},{id:2,title:'p1',price:30},{id:3,title:'p1',price:30}])
   return (
     <div>
-      
-      <h2>Hello World !!!!!</h2>
+       
+<ul>
+       {
+        data.map(p=>(
+          <li key={p.id}>
+            {p.title} ** {p.price}
+          </li>
+        ))
+       }
+       </ul>
     </div>
   )
 }
 
-export default product
+export default Product
